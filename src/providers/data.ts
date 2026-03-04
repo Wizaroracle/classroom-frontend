5920ca1f989fb30b595ec5c57a7c6cfdb654c30a
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-04 21:10:29 +0800

    remove duplicate classes

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,9 +1,3 @@
-// import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-// import { API_URL } from "./constants";
-// export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-//   apiURL: API_URL,
-// });
-
 import {
   BaseRecord,
   DataProvider,
